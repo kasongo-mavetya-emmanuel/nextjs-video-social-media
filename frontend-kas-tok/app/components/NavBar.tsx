57d90d5942d3bbae4c0a9a47072535c58aff5324
kasongo-mavetya-emmanuel <emmanuelkasongomavetya@gmail.com>
2023-07-06 22:51:52 +0200

    components added

---
+++ frontend-kas-tok/app/components/NavBar.tsx
@@ -0,0 +1,16 @@
+export default function NavBar() {
+  return (
+    <div className="border-b border-gray flex flex-col items-center w-full py-5">
+      <div className="max-w-screen-2xl">
+        <h2 className="font-semibold text-xl">KasTok</h2>
+        <div>
+          <input />
+          <button></button>
+        </div>
+        <div>
+          <button>SignIn</button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend-kas-tok/app/components/index.tsx
@@ -0,0 +1,6 @@
+import NavBar from "./NavBar";
+import SideBar from "./SideBar";
+import Content from "./Content";
+import People from "./People";
+
+export { NavBar, SideBar, Content, People };
