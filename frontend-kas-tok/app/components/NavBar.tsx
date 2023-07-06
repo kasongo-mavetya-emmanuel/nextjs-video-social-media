9af47d71d352a0fa94c34031084bac4e5ee2a2e9
kasongo-mavetya-emmanuel <emmanuelkasongomavetya@gmail.com>
2023-07-07 00:03:58 +0200

    navbar added

--- frontend-kas-tok/app/components/NavBar.tsx
+++ frontend-kas-tok/app/components/NavBar.tsx
@@ -1,14 +1,26 @@
+import { BsSearch } from "react-icons/bs";
+import { FcGoogle } from "react-icons/fc";
 export default function NavBar() {
   return (
-    <div className="border-b border-gray flex flex-col items-center w-full py-5">
-      <div className="max-w-screen-2xl">
-        <h2 className="font-semibold text-xl">KasTok</h2>
-        <div>
-          <input />
-          <button></button>
+    <div className="border-b border-gray w-full py-5">
+      <div className="max-w-screen-xl flex justify-between mx-auto items-center">
+        <h2 className="font-bold text-2xl">KasTok</h2>
+        <div className="flex items-center gap-5 border border-gray rounded-full  bg-slate-200 px-5">
+          <input
+            name="Search"
+            aria-label="Search"
+            placeholder="Search"
+            type="Search"
+            required
+            className="px-3 py-3 bg-none bg-transparent focus:outline-none"
+          />
+          <BsSearch />
         </div>
         <div>
-          <button>SignIn</button>
+          <button className="flex items-center gap-2 font-semibold border border-gray px-5 py-3">
+            <FcGoogle size={"2rem"} />
+            Google SignIn
+          </button>
         </div>
       </div>
     </div>
--- frontend-kas-tok/app/page.tsx
+++ frontend-kas-tok/app/page.tsx
@@ -1,16 +1,13 @@
 import { NavBar, SideBar, Content, People } from "./components";
 export default function Home() {
   return (
-    <main className="w-screen h-[100vh] bg-white">
-      <div className="max-w-screen-xl bg-black">
-        <p>dsf</p>
-        {/* <NavBar />
-        <main className="max-w-screen-lg">
-          <SideBar />
-          <Content />
-          <People />
-        </main> */}
-      </div>
+    <main className="w-screen h-[100vh] bg-white overflow-hidden">
+      <NavBar />
+      <main className="max-w-screen-lg mx-auto">
+        <SideBar />
+        <Content />
+        <People />
+      </main>
     </main>
   );
 }
