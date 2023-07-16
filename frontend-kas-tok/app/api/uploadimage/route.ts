f59ff70ec5114ff7b6fa3c9a03671cd04470d5c8
kasongo-mavetya-emmanuel <emmanuelkasongomavetya@gmail.com>
2023-07-16 02:24:07 +0200

    cloudinary upload video logic added

--- frontend-kas-tok/app/api/uploadimage/route.ts
+++ frontend-kas-tok/app/api/uploadimage/route.ts
@@ -2,6 +2,13 @@ import { client } from "@/lib/utils/client";
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/utils/auth";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "dcoebvibe",
+  api_key: "539492991765928",
+  api_secret: "***************************",
+});
 
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
@@ -10,12 +17,11 @@ export async function POST(req: Request) {
     const body: any = req.json();
     console.log("1111111111111111111111111");
     console.log(body);
-    const document = await client.assets.upload("file", body, {
-      filename: body.filename,
-      contentType: body.contentType,
+    const doc = await cloudinary.uploader.upload("dog.mp4", {
+      resource_type: "video",
     });
-
-    return new Response(JSON.stringify(document), { status: 201 });
+    console.log(doc);
+    return new Response(JSON.stringify(doc), { status: 201 });
   }
 
   return new Response("Failed to upload the video", { status: 400 });
