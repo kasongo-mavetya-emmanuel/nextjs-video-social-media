306d3badb6acc8ae60c5ec10cbe41b2977bc1728
kasongo-mavetya-emmanuel <emmanuelkasongomavetya@gmail.com>
2023-07-25 16:10:12 +0200

    sanity client modified

--- frontend-kas-tok/lib/utils/client.ts
+++ frontend-kas-tok/lib/utils/client.ts
@@ -3,7 +3,7 @@ import imageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
-  projectId: process.env.NEXT_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: "production",
   useCdn: false, // set to `false` to bypass the edge cache
   apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
